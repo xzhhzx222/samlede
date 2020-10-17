40dec7928304bc5760cc8db37c0a52274717e2f5
Sam Xu <962226532@qq.com>
2020-10-17 23:07:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,6 +22,5 @@ sed -i 's/OpenWrt/SamLede/g' package/base-files/files/bin/config_generate
 # 修改默认root密码
 sed -i 's#root::0:0:99999:7:::#root:$1$yW9piKyc$OT6rrlpcoPRvf1Vk.Zm9N/:18415:0:99999:7:::#g' package/base-files/files/etc/shadow
 # 更改默认主题
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
