a7ec788bce8c1cd492d6aac2a24ef372d34bb2fd
Sam Xu <962226532@qq.com>
2020-10-17 22:35:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,8 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git diy https://github.com/xiaorouji/openwrt-package.git' feeds.conf.default
+sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
