beb1adfe4425da66e4b0ece25353fd8d3e044ee0
Sam Xu <962226532@qq.com>
2020-10-17 23:20:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,6 +24,6 @@ sed -i 's/17.01/19.07/g' feeds.conf.default
 
 # 添加ssr软件包
 git clone https://github.com/coolsnowwolf/lede.git package/lean1
-mv -f package/lean1/package/lean/microsocks package/lean
+#mv -f package/lean1/package/lean/microsocks package/lean
 mv -f package/lean1/package/lean/redsocks2 package/lean
 rm -rf package/lean1
