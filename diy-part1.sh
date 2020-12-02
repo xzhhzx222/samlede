4627c0a7d928520eb6c183588d3273c6c2bf078f
Sam Xu <962226532@qq.com>
2020-12-02 10:35:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,6 @@ sed -i '$a src-git jerrykuku https://github.com/jerrykuku/luci-app-vssr.git' fee
 #sed -i 's/17.01/19.07/g' feeds.conf.default
 
 # 添加ssr软件包
-#git clone https://github.com/coolsnowwolf/lede.git package/lean1
-#mv -f package/lean1/package/lean/microsocks package/lean
-#mv -f package/lean1/package/lean/redsocks2 package/lean
-#rm -rf package/lean1
+git clone https://github.com/coolsnowwolf/lede.git package/lean1
+mv -f package/lean1/package/lean/redsocks2 package/lean
+rm -rf package/lean1
