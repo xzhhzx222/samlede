78973b9f474e55cd452950d8e313a644285532c5
Sam Xu <962226532@qq.com>
2020-12-05 16:48:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# 移动samba4到nas下
-#sed -i 's/"services"/"nas"/g' package/lean/luci-app-samba4/luasrc/controller/samba4.lua
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 # 修改默认主机名称
@@ -22,8 +20,10 @@ sed -i 's/OpenWrt/SamLede/g' package/base-files/files/bin/config_generate
 # 修改默认root密码
 sed -i 's#root::0:0:99999:7:::#root:$1$yW9piKyc$OT6rrlpcoPRvf1Vk.Zm9N/:18415:0:99999:7:::#g' package/base-files/files/etc/shadow
 # 更改默认主题
-#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon-light-mod/g' feeds/luci/collections/luci/Makefile
+# 添加helloworld
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/helloworld
 # 添加redsocks2
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/lean/redsocks2
 # 替换libcap
