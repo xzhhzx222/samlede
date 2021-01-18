1131196655ca2d99095a8690ad32d6d9dd97c306
Sam Xu <962226532@qq.com>
2021-01-19 00:44:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,25 +25,22 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 # 取消53端口监听
 sed -i 's/iptables/#&/' package/lean/default-settings/files/zzz-default-settings
-# 添加argon-config
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-# 添加advanced
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/sirpdboy/advanced
-# 添加lienol diy
-#svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/diy/luci-app-adguardhome
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/OpenAppFilter package/diy/OpenAppFilter
 # 添加adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-# 添加openappfilter
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-# 添加smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-# 添加dnsfilter
-#git clone https://github.com/garypang13/luci-app-dnsfilter.git package/garypang/dnsfilter
+# 添加advanced
+git clone https://github.com/sirpdboy/luci-app-advanced.git package/sirpdboy/advanced
+# 添加argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 # 添加bypass
 #svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/garypang/lua-maxmindb
 #svn co https://github.com/garypang13/openwrt-packages/trunk/redsocks2 package/garypang/redsocks2
 #git clone https://github.com/garypang13/luci-app-bypass.git package/garypang/bypass
+# 添加dnsfilter
+#git clone https://github.com/garypang13/luci-app-dnsfilter.git package/garypang/dnsfilter
+# 添加openappfilter
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# 添加smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # sfe开启bbr
 sed -i "s/option bbr '0'"/"option bbr '1'/g" package/lean/luci-app-sfe/root/etc/config/sfe
 # 修改ntp服务器
