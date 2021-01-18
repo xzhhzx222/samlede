5fe61cdfb3c0b134f7ecc2cef220cce5ab8582c2
Sam Xu <962226532@qq.com>
2021-01-18 18:37:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -34,9 +34,6 @@ git clone https://github.com/garypang13/luci-app-dnsfilter.git package/garypang/
 svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/garypang/lua-maxmindb
 svn co https://github.com/garypang13/openwrt-packages/trunk/redsocks2 package/garypang/redsocks2
 git clone https://github.com/garypang13/luci-app-bypass.git package/garypang/luci-app-bypass
-# 添加docker
-svn co https://github.com/coolsnowwolf/packages/trunk/utils/docker-ce package/docker/docker-ce
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-docker package/docker/luci-app-docker
 # 替换libcap
 #rm -rf feeds/packages/libs/libcap
 #svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
