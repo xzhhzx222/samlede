6ed68ce043a2e39741ecef43041f66a0e20db4bc
Sam Xu <962226532@qq.com>
2021-01-18 19:21:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git' feeds.c
 #sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
 sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 #sed -i '$a src-git jerrykuku https://github.com/jerrykuku/luci-app-vssr.git' feeds.conf.default
+sed -i '$a src-git lisaac https://github.com/lisaac/luci-app-dockerman.git' feeds.conf.default
