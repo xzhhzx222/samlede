9d5fd36eecd65828928997f2ccbb68585b6bdb90
Sam Xu <962226532@qq.com>
2021-01-19 15:44:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,3 @@
 #sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
 sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 sed -i '$a src-git lisaac https://github.com/lisaac/luci-app-dockerman.git' feeds.conf.default
-# 添加adguardhome
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-svn co https://github.com/Lienol/openwrt-packages/trunk/net/adguardhome feeds/AdGuardhome/adguarhome
-svn co https://github.com/Lienol/openwrt-packages/trunk/devel/packr feeds/AdGuardhome/packr
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/luci-app-adguarhome
