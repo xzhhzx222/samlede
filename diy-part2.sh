0ef25f4d5905d402cf5db03b695fe235c8454297
Sam Xu <962226532@qq.com>
2021-01-19 14:35:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,6 +28,7 @@ sed -i 's/iptables/#&/' package/lean/default-settings/files/zzz-default-settings
 # 添加adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/adguardhome package/AdGuardhome/adguarhome
+svn co https://github.com/Lienol/openwrt-packages/trunk/devel/packr package/AdGuardhome/packr
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/AdGuardhome/luci-app-adguarhome
 # 添加advanced
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/sirpdboy/advanced
