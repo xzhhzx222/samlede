1e39c3e52d54f33e3569ace3b1931429c628eb52
Sam Xu <962226532@qq.com>
2021-01-19 17:11:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,23 +28,22 @@ sed -i 's/iptables/#&/' package/lean/default-settings/files/zzz-default-settings
 # 添加adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 # 添加advanced
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/advanced
+git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
 # 添加argon-config
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 # 添加bypass
-#svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/bypass/lua-maxmindb
-#git clone https://github.com/garypang13/luci-app-bypass.git package/bypass/luci-app-bypass
+git clone https://github.com/garypang13/luci-app-bypass.git package/bypass/luci-app-bypass
 # 添加dnsfilter
 git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 # 添加eqos
 git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
-# 添加openappfilter
-git clone https://github.com/destan19/OpenAppFilter.git package/openappfilter
 # 添加smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # 添加hellowworld
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/vssr/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/vssr/luci-app-vssr
+# 添加openappfilter
+git clone https://github.com/destan19/OpenAppFilter.git package/openappfilter
 # sfe开启bbr
 sed -i "s/option bbr '0'"/"option bbr '1'/g" package/lean/luci-app-sfe/root/etc/config/sfe
 # 修改ntp服务器
