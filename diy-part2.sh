1b66d308fd0a9ea9f64bdb6635030c18c62465b5
Sam Xu <962226532@qq.com>
2021-01-22 09:33:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,7 +33,7 @@ git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-adv
 # 添加bypass
 #git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
 # 添加dnsfilter
-#git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
+git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 # 添加eqos
 #git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
 # 添加helloworld
