bd4ff67987210dfe87b23d121c960a4b797b2acb
Sam Xu <962226532@qq.com>
2021-01-24 20:19:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,6 +27,9 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package
 # 替换libcap
 #rm -rf package/feeds/packages/libcap
 #svn co https://github.com/openwrt/packages/trunk/libs/libcap package/libcap
+# 替换luci-lib-docker
+rm -rf package/diy/luci-lib-docker
+git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
 
 # 添加advanced
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
