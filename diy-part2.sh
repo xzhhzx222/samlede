f8971952aa8cf35418173e83f1cdae4049c54a85
Sam Xu <962226532@qq.com>
2021-01-25 11:29:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,6 +29,18 @@ sed -i 's/iptables/#&/' package/lean/default-settings/files/zzz-default-settings
 sed -i '0,/REJECT/s//ACCEPT/' package/network/config/firewall/files/firewall.config
 # 默认开启upnp
 sed -i '0,/0/s//1/' package/feeds/packages/miniupnpd/files/upnpd.config
+# sfe开启bbr
+sed -i "s/option bbr '0'"/"option bbr '1'/g" package/lean/luci-app-sfe/root/etc/config/sfe
+# 修改ntp服务器
+#sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+#sed -i 's/1.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
+#sed -i 's/2.openwrt.pool.ntp.org/time.ustc.edu.cn/g' package/base-files/files/bin/config_generate
+#sed -i 's/3.openwrt.pool.ntp.org/cn.pool.ntp.org/g' package/base-files/files/bin/config_generate
+# dnsmasq支持iptv
+sed -i '$a dhcp-option-force=125,00:00:00:00:1a:02:06:48:47:57:2d:43:54:03:04:5a:58:48:4e:0a:02:20:00:0b:02:00:55:0d:02:00:2e' package/network/services/dnsmasq/files/dnsmasq.conf
+sed -i '$a dhcp-option=15' package/network/services/dnsmasq/files/dnsmasq.conf
+sed -i '$a dhcp-option=28' package/network/services/dnsmasq/files/dnsmasq.conf
+sed -i '$a dhcp-option=60,00:00:01:06:68:75:61:71:69:6E:02:0A:48:47:55:34:32:31:4E:20:76:33:03:0A:48:47:55:34:32:31:4E:20:76:33:04:10:32:30:30:2E:55:59:59:2E:30:2E:41:2E:30:2E:53:48:05:04:00:01:00:50' package/network/services/dnsmasq/files/dnsmasq.conf
 
 # 添加adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
@@ -52,18 +64,6 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git package/vssr/luci-app-v
 # 添加ttnode
 git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
 
-# sfe开启bbr
-sed -i "s/option bbr '0'"/"option bbr '1'/g" package/lean/luci-app-sfe/root/etc/config/sfe
-# 修改ntp服务器
-#sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
-#sed -i 's/1.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
-#sed -i 's/2.openwrt.pool.ntp.org/time.ustc.edu.cn/g' package/base-files/files/bin/config_generate
-#sed -i 's/3.openwrt.pool.ntp.org/cn.pool.ntp.org/g' package/base-files/files/bin/config_generate
-# dnsmasq.conf支持iptv
-sed -i '$a dhcp-option-force=125,00:00:00:00:1a:02:06:48:47:57:2d:43:54:03:04:5a:58:48:4e:0a:02:20:00:0b:02:00:55:0d:02:00:2e' package/network/services/dnsmasq/files/dnsmasq.conf
-sed -i '$a dhcp-option=15' package/network/services/dnsmasq/files/dnsmasq.conf
-sed -i '$a dhcp-option=28' package/network/services/dnsmasq/files/dnsmasq.conf
-sed -i '$a dhcp-option=60,00:00:01:06:68:75:61:71:69:6E:02:0A:48:47:55:34:32:31:4E:20:76:33:03:0A:48:47:55:34:32:31:4E:20:76:33:04:10:32:30:30:2E:55:59:59:2E:30:2E:41:2E:30:2E:53:48:05:04:00:01:00:50' package/network/services/dnsmasq/files/dnsmasq.conf
 # 移动accesscontrol到网络下
 sed -i 's/"services"/"network"/g' package/lean/luci-app-accesscontrol/luasrc/controller/mia.lua
 sed -i 's/services/network/g' package/lean/luci-app-accesscontrol/luasrc/view/mia/mia_status.htm
@@ -80,4 +80,3 @@ sed -i 's/services/network/g' package/feeds/luci/luci-app-upnp/luasrc/view/upnp_
 #sed -i 's/"services"/"nas"/g' package/feeds/luci/luci-app-samba/luasrc/controller/samba.lua
 # 移动samba4到Nas下
 #sed -i 's/"services"/"nas"/g' package/lean/luci-app-samba4/luasrc/controller/samba4.lua
-
