fda11ba4d027195b9d0e7752e3214504851d532c
Sam Xu <962226532@qq.com>
2021-02-04 15:52:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,7 @@
 # Add a feed source
 # 添加openclash
 #sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
+# 添加passwall
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 # 添加ssr plus
 sed -i '$a src-git ssrplus https://github.com/fw876/helloworld' feeds.conf.default
