ce73937026f2101313d4e574d04b8447b5cf4cdc
Sam Xu <962226532@qq.com>
2021-02-13 20:07:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,3 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# 添加openclash
-#sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
-# 添加passwall
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
-# 添加ssr plus
-sed -i '$a src-git ssrplus https://github.com/fw876/helloworld' feeds.conf.default
